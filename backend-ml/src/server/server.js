f4d598272698c07bd6485ece89f99d7bf8e7833f
c006d4ky0850 <c006d4ky0850@bangkit.academy>
2024-06-13 00:55:17 +0000

    updated server.js backendml

--- backend-ml/src/server/server.js
+++ backend-ml/src/server/server.js
@@ -6,8 +6,9 @@ const InputError = require("../exceptions/InputError");
 
 (async () => {
   const server = Hapi.server({
-    port: 8081,
+    port: 8080,
     host: "0.0.0.0",
+    // host: localhost,
     routes: {
       cors: {
         origin: ["*"],
